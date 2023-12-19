9784b45bbfcc76fd3b37fa88f4a973fb3aa2537f
Nayana333 <nrnayana1@gmail.com>
2023-12-19 15:48:18 +0530

    chart set

--- controller/adminController.js
+++ controller/adminController.js
@@ -116,14 +116,149 @@ const adminDashboard=async(req,res)=>{
     ]);
 
 
+    /////daily/////////////
+    
+    const getDailyDataArray = async () => {
+        //weekly data
+        const currentDate = new Date(); 
+        const sevenDaysAgo = new Date(currentDate); 
+        sevenDaysAgo.setDate(currentDate.getDate() - 7); 
+        
+        const dailyOrders = await Order.aggregate([
+          {
+            $match: {
+                status:"payment successfull",
+              orderDate: { $gte: sevenDaysAgo, $lte: currentDate } 
+            }
+          },
+          {
+            $group: {
+              _id: { $dayOfWeek: '$orderDate' },
+              count: { $sum: 1 }
+            }
+          },
+          {
+            $sort: { '_id': 1 } 
+          }
+        ]);
+        
+        const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        
+        const dailyDataArray = [];
+        for (let i = 6; i >= 0; i--) {
+          const dayIndex = new Date(currentDate);
+          dayIndex.setDate(currentDate.getDate() - i); 
+          const foundDay = dailyOrders.find(order => order._id === (dayIndex.getDay() === 0 ? 7 : dayIndex.getDay())); 
+          const count = foundDay ? foundDay.count : 0; 
+          const dayNameIndex = dayIndex.getDay() === 0 ? 6 : dayIndex.getDay() - 1; 
+          const dayName = dayNames[dayNameIndex]; 
+          dailyDataArray.push({ day: dayName, count }); 
+        }
+        
+        
+        return dailyDataArray;
+        
+        }
+
+        ////monthly////
+        const getMonthlyDataArray = async () => {
+
+            //Monthly Data
+            const currentDate = new Date(); 
+            const sevenMonthsAgo = new Date(); 
+            sevenMonthsAgo.setMonth(sevenMonthsAgo.getMonth() - 7);
+            
+            const monthlyOrders = await Order.aggregate([
+              {
+                $match: {
+                    status:"payment successfull",
+                  orderDate: { $gte: sevenMonthsAgo, $lte: currentDate }
+                }
+              },
+              {
+                $group: {
+                  _id: { $month: '$orderDate' },
+                  count: { $sum: 1 }
+                }
+              },
+              {
+                $sort: { '_id': 1 } 
+              }
+            ]);
+            console.log(monthlyOrders+'monthly');
+            
+            const monthNames = [
+              'January', 'February', 'March', 'April', 'May', 'June',
+              'July', 'August', 'September', 'October', 'November', 'December'
+            ];
+            
+            const monthlyDataArray = [];
+            for (let i = 6; i >= 0; i--) {
+              const monthIndex = (currentDate.getMonth() - i + 12) % 12; 
+              const foundMonth = monthlyOrders.find(order => order._id === (monthIndex + 1)); 
+              const count = foundMonth ? foundMonth.count : 0; 
+              const monthName = monthNames[monthIndex];
+              monthlyDataArray.push({ month: monthName, count }); 
+            }
+            
+            return monthlyDataArray;
+            };
+
+            /////yearly////
+
+            const getYearlyDataArray = async () => {
+                const currentDate = new Date(); 
+                const sevenYearsAgo = new Date(currentDate); 
+                sevenYearsAgo.setFullYear(currentDate.getFullYear() - 7); 
+                
+                const yearlyOrders = await Order.aggregate([
+                  {
+                    $match: {
+                        status:"payment successfull",
+                      orderDate: { $gte: sevenYearsAgo, $lte: currentDate } 
+                    }
+                  },
+                  {
+                    $group: {
+                      _id: { $year: '$orderDate' },
+                      count: { $sum: 1 }
+                    }
+                  },
+                  {
+                    $sort: { '_id': 1 } 
+                  }
+                ]);
+                
+                
+                const yearlyDataArray = [];
+                for (let i = 6; i >= 0; i--) {
+                  const year = currentDate.getFullYear() - i; 
+                
+                  const foundYear = yearlyOrders.find(order => order._id === year); 
+                  const count = foundYear ? foundYear.count : 0; 
+                  yearlyDataArray.push({ year, count }); 
+                }
+                
+                
+                return yearlyDataArray;
+                };
+
+
+
     console.log('monthlyEarnings'+monthlyEarnings);
     const adminData=req.session.adminData
     const totalRevenueValue=totalRevenue.length > 0 ?totalRevenue[0].totalAmount : 0;
     const monthlyEarningsValue=monthlyEarnings.length > 0 ?monthlyEarnings[0].monthlyAmount : 0;
     console.log(monthlyEarningsValue);
-    const monthlyDataArray=await charData.getMonthlyDataArray();
-    const dailyDataArray=await charData.getDailyDataArray();
-    const yearlyDataArray=await charData.getYearlyDataArray();
+    const monthlyDataArray=await getMonthlyDataArray();
+    const dailyDataArray=await getDailyDataArray();
+    const yearlyDataArray=await getYearlyDataArray();
+
+    console.log(monthlyDataArray);
+    console.log(dailyDataArray);
+    console.log(yearlyDataArray);
+
+
     
 
         var search='';
@@ -141,6 +276,7 @@ const adminDashboard=async(req,res)=>{
     });
     console.log(totalRevenueValue+'total');
         res.render('dashboard',{admin: adminData,
+         data,
         orders,
         newUsers,
         totalRevenue: totalRevenueValue,
@@ -156,6 +292,18 @@ const adminDashboard=async(req,res)=>{
         yearlyYears: yearlyDataArray.map(item => item.year),
         yearlyOrderCounts: yearlyDataArray.map(item => item.count),
         })
+
+
+
+
+
+          
+
+
+               
+            
+            
+            
 
 
     }catch(error){
